09c26530fa4bc068b3ced300d177f6d1d75fb94c
miguel444 <miguel.a98@hotmail.es>
2020-10-19 12:53:57 +0200

    Repositorio actualizado

--- my_module/js/form_result.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  // Argument passed from InvokeCommand.
-  $.fn.AjaxCallback = function(argument) {
-
-    console.log(argument);
-    // Set textfield's value to the passed arguments.
-    //$('input#edit-deporte').attr('options', argument);
-  };
-})(jQuery);
